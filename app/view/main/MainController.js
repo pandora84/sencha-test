1d11f8996198e0247e7c4b10eb0a3302da3fdc63
Nataliya Zhdanova <nataliya.zhdanova@VEGAECM.COM>
2017-04-03 16:47:30 +0300

    second build

--- app/view/main/MainController.js
+++ app/view/main/MainController.js
@@ -15,7 +15,44 @@ Ext.define('Sencha.test.view.main.MainController', {
 
     onConfirm: function (choice) {
         if (choice === 'yes') {
-            //
+Ext.create('Ext.window.Window',{
+    title:'My Window',
+    modal:true,
+    width:200,
+    height:200,
+    items:[
+      Ext.create('Ext.grid.Panel', {
+        renderTo: document.body,
+        selModel: Ext.create('Ext.selection.CheckboxModel', {
+          mode: 'MULTI',
+          checkOnly: true,
+          showHeaderCheckbox: false,
+          listeners: {
+            select: function(){
+              console.log('selected');    
+            },
+            deselect: {
+                fn: function() {
+                   Ext.Msg.alert('Deselect', 'Do you really want to do this?');
+                   console.log('deselected');    
+                },
+                delay: 1
+            }
+          }
+        }),
+        store: {
+           fields: ['a'],
+           data: [{ a: 1 }, { a: 2 }, { a: 3 }]
+        },
+        columns: [{
+            text: 'Column Header',
+            dataIndex: 'a',
+            flex:1
+         }]
+      })
+    ]
+  }).show();
+
         }
     }
 });
